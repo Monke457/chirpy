197b5ca860ff504f0c0e100615872a64e3f63ea8
Beth <beth.williams@bluewin.ch>
2024-09-11 12:03:06 +0200

    metrics

--- main.go
+++ main.go
@@ -5,12 +5,21 @@ import (
 	"net/http"
 )
 
-func main() {
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.HandleFunc("/healthz", healthCheck)
+type apiConfig struct {
+	fileserverHits int
+}
 
-	server := http.Server{ Handler: serveMux, Addr: ":8080" }
+func main() {
+	cfg := apiConfig{ fileserverHits: 0 }
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("GET /api/healthz", healthCheck)
+	mux.HandleFunc("GET /api/metrics", cfg.getMetrics)
+	mux.HandleFunc("/api/reset", cfg.resetMetrics)
+
+	server := http.Server{ Handler: mux, Addr: ":8080" }
 	
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Println(err)
@@ -23,4 +32,23 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset-utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset-utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Metrics reset"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+    })
 }
